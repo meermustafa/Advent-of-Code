c0650065e144bf7f3e5edbf54145a9b0da2d2bed
Meer Mustafa <meer.k.mustafa@gmail.com>
2024-05-23 21:17:10 -0700

    day 4

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -27,9 +27,13 @@ def prepare_cards_into_list_of_numbers(card):
 
 
 game_score = []
+matched_numbers_count_list = []
+d = {}
 for line in file.split('\n'):
     print(line) #
     id, cards = line.split(':')
+    id_num = id.split(' ')[1]
+    print(id_num)
     winning_card, my_card = cards.split('|')
     winning_card, my_card = prepare_cards_into_list_of_numbers(winning_card), prepare_cards_into_list_of_numbers(my_card)
     print(f"winning_card is {winning_card}, \n my_card is {my_card} \n") #
@@ -37,6 +41,7 @@ for line in file.split('\n'):
     matched_numbers = [matched_num for matched_num in my_card if matched_num in winning_card]
     print(f"matched_numbers are {matched_numbers} = {len(matched_numbers)} number of winning numbers.")
     matched_numbers_count = len(matched_numbers)
+    matched_numbers_count_list.append(matched_numbers_count)
     if matched_numbers_count == 0:
         score = 0
     if matched_numbers_count == 1:
@@ -47,8 +52,29 @@ for line in file.split('\n'):
     print(score)
     game_score.append(score)
 
-print(game_score)
-print(len(game_score))
+
+    # part 2
+    matched_numbers_count
+
+
+
+
+# print(game_score)
+# print(len(game_score))
 print(sum(game_score))
 
+# d = {} for k, v in  len(game_score), matched_numbers_count_list:
+#     d[k] = v
+
+# original card scores (e.g. card 1 has 2 matches)
+d = dict(zip(range(len(matched_numbers_count_list)), matched_numbers_count_list))
+
+# make a copy of the original card scores dictionary to hold new "copy" card scores
+d1 = d
+print(d1)
+for k, v in d1:
+    # use original cards calculate 
+
+def get_unique_characters(string):
+    
 # if __name == main():
